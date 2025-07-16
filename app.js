d2e19bfa128ba68e7474f19ad8039cfd66cb60f3
Krishna Kant Narayan <80309096+krishna512-code@users.noreply.github.com>
2025-07-16 22:17:55 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const socketio = require("socket.io");
+const path = require("path");
+
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket) {
+    socket.on("send-location", function(data) {
+        io.emit("receive-location", { id: socket.id, ...data });
+    });
+    socket.on("disconnect", function() {
+        io.emit("user-disconnect", socket.id);
+    });
+});
+
+
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+server.listen(3000);
